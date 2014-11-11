8ea00affa835609038bb54663c50c92a39b797c7
EdmarJr <edmarfagunde@gmail.com>
2014-11-11 11:17:23 -0200

    ajustes

---
+++ dspace/etc/postgres/pos-migracao.sql
@@ -0,0 +1,101 @@
+delete from community2community;
+update community SET name = 'Toda a BDJur' where community_id = 4; 
+update handle set handle = '123456789/25' where handle = '2011/8';
+
+update handle set handle = 'tempedmar' where handle = '2011/17962';
+update handle set handle = '2011/17962' where handle = '123456789/1';
+update handle set handle = '123456789/1' where handle = 'tempedmar';
+
+update handle set handle = 'tempedmar' where handle = '2011/71373';
+update handle set handle = '2011/71373' where handle = '123456789/4';
+update handle set handle = '123456789/4' where handle = 'tempedmar';
+
+update handle set handle = 'tempedmar' where handle = '2011/71373';
+update handle set handle = '2011/71373' where handle = '123456789/4';
+update handle set handle = '123456789/4' where handle = 'tempedmar';
+
+update handle set handle = 'tempedmar' where handle = '2011/14';
+update handle set handle = '2011/14' where handle = '123456789/5';
+update handle set handle = '123456789/5' where handle = 'tempedmar';
+
+update handle set handle = 'tempedmar' where handle = '2011/64248';
+update handle set handle = '2011/64248' where handle = '123456789/6';
+update handle set handle = '123456789/6' where handle = 'tempedmar';
+
+update handle set handle = 'tempedmar' where handle = '2011/9718';
+update handle set handle = '2011/9718' where handle = '123456789/7';
+update handle set handle = '123456789/7' where handle = 'tempedmar';
+
+update handle set handle = 'tempedmar' where handle = '2011/17968';
+update handle set handle = '2011/17968' where handle = '123456789/2';
+update handle set handle = '123456789/2' where handle = 'tempedmar';
+
+update handle set handle = 'tempedmar' where handle = '2011/18084';
+update handle set handle = '2011/18084' where handle = '123456789/8';
+update handle set handle = '123456789/8' where handle = 'tempedmar';
+
+update handle set handle = 'tempedmar' where handle = '2011/26655';
+update handle set handle = '2011/26655' where handle = '123456789/9';
+update handle set handle = '123456789/9' where handle = 'tempedmar';
+
+update handle set handle = 'tempedmar' where handle = '2011/66542';
+update handle set handle = '2011/66542' where handle = '123456789/10';
+update handle set handle = '123456789/10' where handle = 'tempedmar';
+
+update handle set handle = 'tempedmar' where handle = '2011/29753';
+update handle set handle = '2011/29753' where handle = '123456789/11';
+update handle set handle = '123456789/11' where handle = 'tempedmar';
+
+update handle set handle = 'tempedmar' where handle = '2011/8886';
+update handle set handle = '2011/8886' where handle = '123456789/12';
+update handle set handle = '123456789/12' where handle = 'tempedmar';
+
+update handle set handle = 'tempedmar' where handle = '2011/74765';
+update handle set handle = '2011/74765' where handle = '123456789/13';
+update handle set handle = '123456789/13' where handle = 'tempedmar';
+
+update handle set handle = 'tempedmar' where handle = '2011/9';
+update handle set handle = '2011/9' where handle = '123456789/14';
+update handle set handle = '123456789/14' where handle = 'tempedmar';
+
+update handle set handle = 'tempedmar' where handle = '2011/3231';
+update handle set handle = '2011/3231' where handle = '123456789/15';
+update handle set handle = '123456789/15' where handle = 'tempedmar';
+
+update handle set handle = 'tempedmar' where handle = '2011/1938';
+update handle set handle = '2011/1938' where handle = '123456789/16';
+update handle set handle = '123456789/16' where handle = 'tempedmar';
+
+update handle set handle = 'tempedmar' where handle = '2011/12';
+update handle set handle = '2011/12' where handle = '123456789/17';
+update handle set handle = '123456789/17' where handle = 'tempedmar';
+
+update handle set handle = 'tempedmar' where handle = '2011/19574';
+update handle set handle = '2011/19574' where handle = '123456789/3';
+update handle set handle = '123456789/3' where handle = 'tempedmar';
+
+update handle set handle = 'tempedmar' where handle = '2011/51302';
+update handle set handle = '2011/51302' where handle = '123456789/18';
+update handle set handle = '123456789/18' where handle = 'tempedmar';
+
+update handle set handle = 'tempedmar' where handle = '2011/51301';
+update handle set handle = '2011/51301' where handle = '123456789/19';
+update handle set handle = '123456789/19' where handle = 'tempedmar';
+
+update handle set handle = 'tempedmar' where handle = '2011/20276';
+update handle set handle = '2011/20276' where handle = '123456789/20';
+update handle set handle = '123456789/20' where handle = 'tempedmar';
+
+update handle set handle = 'tempedmar' where handle = '2011/54762';
+update handle set handle = '2011/54762' where handle = '123456789/21';
+update handle set handle = '123456789/21' where handle = 'tempedmar';
+
+update handle set handle = 'tempedmar' where handle = '2011/47078';
+update handle set handle = '2011/47078' where handle = '123456789/22';
+update handle set handle = '123456789/22' where handle = 'tempedmar';
+
+
+
+
+
+
